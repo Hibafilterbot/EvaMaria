8b3e7bc9ef34c238a20d089d207a7dbcdf1d8143
Hibafilterbot <106365259+Hibafilterbot@users.noreply.github.com>
2022-08-27 21:57:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -406,8 +406,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
-            InlineKeyboardButton('🎭ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ🎭', url='https://t.me/moviesclubz77') 
-            InlineKeyboardButton('💐ʙᴏᴛ ᴏᴡɴᴇʀ💐',url='https://t.me/moviesclubowne') 
+            InlineKeyboardButton('ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ', url='https://t.me/moviesclubz77') 
+            InlineKeyboardButton('ʙᴏᴛ ᴏᴡɴᴇʀ',url='https://t.me/moviesclubowne') 
         ], [
             InlineKeyboardButton('🔍 sᴇᴀʀᴄʜ', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇs', url='https://t.me/+yvxJ0OnUUDs3NzRl')
